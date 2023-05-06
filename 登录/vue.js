0208d634d259121d4384255bfe71036ce3fc6e0b
I-am-shy <119798746+I-am-shy@users.noreply.github.com>
2023-05-06 12:45:51 +0800

    Update vue.js

--- 登录/vue.js
+++ 登录/vue.js
@@ -1,4 +1,4 @@
 var login = document.getElementById("login");
 login.addEventListener("click",function(e){
-    window.location.href = "index.html";//跳转页面
+    window.location.href = "https://i-am-shy.github.io/page/";//跳转页面
 })
